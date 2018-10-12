f5b2d1d02a5c46b13ee264ac7a658dce855c999b
Benjamin BOUDIER <benjamin.boudier@idci-consulting.fr>
2018-10-12 08:57:30 +0200

    Update: bundle definition

--- IDCIGraphQLClientBundle.php
+++ IDCIGraphQLClientBundle.php
@@ -2,8 +2,22 @@
 
 namespace IDCI\Bundle\GraphQLClientBundle;
 
+use IDCI\Bundle\GraphQLClientBundle\DependencyInjection\Compiler\GraphQLApiClientCompilerPass;
+use IDCI\Bundle\GraphQLClientBundle\DependencyInjection\IDCIGraphQLClientExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class IDCIGraphQLClientBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new GraphQLApiClientCompilerPass());
+    }
+
+    public function getContainerExtension()
+    {
+        return new IDCIGraphQLClientExtension();
+    }
 }
