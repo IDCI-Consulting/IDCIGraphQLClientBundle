34eed0375c9f42ff3b8d430264ade0ed2fcc0d42
Benjamin BOUDIER <benjamin.boudier@idci-consulting.fr>
2018-10-12 08:56:06 +0200

    Update: Merge GraphQLRepository into GraphQLApiClient

--- Client/GraphQLApiClient.php
+++ Client/GraphQLApiClient.php
@@ -2,10 +2,11 @@
 
 namespace IDCI\Bundle\GraphQLClientBundle\Client;
 
+use GraphQL\Graph;
 use GuzzleHttp\ClientInterface;
 use IDCI\Bundle\GraphQLClientBundle\Handler\CacheHandlerInterface;
 
-class GraphQLApiClient
+class GraphQLApiClient implements GraphQLApiClientInterface
 {
     /**
      * @var RedisCacheHandler
@@ -15,23 +16,32 @@ class GraphQLApiClient
     /**
      * @var ClientInterface
      */
-    private $client;
+    private $httpClient;
 
     public function __construct(ClientInterface $httpClient, CacheHandlerInterface $cache)
     {
-        $this->cache = $cache;
         $this->httpClient = $httpClient;
+        $this->cache = $cache;
+    }
+
+    public function buildQueryString(string $action, array $requestedFields, array $parameters = []): string
+    {
+        $graphQlQuery = new Graph($action, $parameters);
+        array_walk($requestedFields, [$this, 'buildGraph'], $graphQlQuery);
+
+        return $this->decodeGraphQLQuery($graphQlQuery);
     }
 
     public function query(string $action, array $requestedFields, array $parameters = []): array
     {
+        $graphQlQuery = $this->buildQueryString($action, $requestedFields, $parameters);
         $graphQlQueryHash = $this->cache->generateHash($graphQlQuery);
 
         if ($this->cache->isCached($graphQlQueryHash)) {
             return json_decode($this->cache->get($graphQlQueryHash), true);
         }
 
-        $response = $this->httpClient->post([
+        $response = $this->httpClient->request('POST', [
             'query' => [
                 'query' => $graphQlQuery,
             ],
@@ -46,5 +56,22 @@ class GraphQLApiClient
         $this->cache->set($graphQlQueryHash, json_encode($result['data']));
 
         return $result['data'];
+    }
+
+    private function decodeGraphQLQuery(string $graphQlQuery)
+    {
+        return preg_replace_callback('/\\\\u([a-f0-9]{4})/', function ($param) {
+            return iconv('UCS-4LE', 'UTF-8', pack('V', hexdec(sprintf('U%s', $param[0]))));
+        }, $graphQlQuery);
+    }
+
+    private function buildGraph($field, $key, &$graphQlQuery)
+    {
+        if (is_array($field)) {
+            $graphQlQuery->$key;
+            array_walk($field, [$this, 'buildGraph'], $graphQlQuery->$key);
+        } else {
+            $graphQlQuery->use($field);
+        }
     }
 }
