2c6a967af2bd4094ee29d13672b0a50775ae176d
Benjamin BOUDIER <benjamin.boudier@idci-consulting.fr>
2018-10-23 10:15:24 +0200

    Update: purge cache command with recent changes

---
+++ Command/PurgeGraphQLCacheCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace IDCI\Bundle\GraphQLClientBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+
+class PurgeGraphQLCacheCommand extends ContainerAwareCommand
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('cache:graphql:clear')
+            ->setDescription('Clear graphql query cache')
+            ->setHelp('Clear results of graphql queries in cache')
+            ->setHelp(
+                <<<EOT
+The <info>%command.name%</info> command.
+Here is an example:
+
+# Clear graphql queries cache
+<info>php bin/console %command.name%</info>
+EOT
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $clients = $this->getContainer()->getParameter('idci_graphql_client.clients');
+        $helper = $this->getHelper('question');
+
+        $question = new ChoiceQuestion(
+            'Please select the graphql client',
+            array_keys($clients),
+            0
+        );
+
+        $question->setErrorMessage('%s is an invalid choice.');
+        $clientName = $helper->ask($input, $output, $question);
+
+        if (!isset($clients[$clientName]['cache'])) {
+            $output->writeln(sprintf('<error>No cache found for client "%s"</error>', $clientName));
+            exit;
+        }
+
+        $this->getContainer()->get($clients[$clientName]['cache'])->clear();
+        $this->getContainer()->get($clients[$clientName]['cache'])->commit();
+
+        $output->writeln(sprintf('<info>Cache cleared for client "%s"</info>', $clientName));
+    }
+}
