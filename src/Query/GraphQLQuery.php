257666ff6e87c92fce25046a9ff0d83375e39eb6
Vincent DUVIVIER <vincent.duvivier@idci-consulting.fr>
2025-03-31 11:08:57 +0200

    fixes

--- src/Query/GraphQLQuery.php
+++ src/Query/GraphQLQuery.php
@@ -9,8 +9,8 @@ use Symfony\Component\HttpFoundation\File\File;
 
 class GraphQLQuery
 {
-    public const string MUTATION_TYPE = 'mutation';
-    public const string QUERY_TYPE = 'query';
+    public const MUTATION_TYPE = 'mutation';
+    public const QUERY_TYPE = 'query';
 
     private string $type;
     private string|array $action;
